ce782fff2d6a3db4aafab3aa204ba7a8f77a825d
MykhailoVobolis <mvobolis@gmail.com>
2024-09-02 23:30:39 +0300

    setting image modal

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -9,7 +9,7 @@ export default function ImageGallery({ gallery, onClick }) {
     <div>
       <ul className={css.galleryList}>
         {gallery.map((item) => (
-          <li key={nanoid()}>
+          <li className={css.galleryItem} key={nanoid()}>
             <ImageCard image={item} onClick={onClick} />
           </li>
         ))}
